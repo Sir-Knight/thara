a43de3b3b6317446d6a1ce09bf3a44d181c0b672
Viranpat Vongswasdi <viranpat072@hotmail.com>
2018-01-05 02:46:42 +0700

    fix import

--- src/components/carouselview/CarouselView.js
+++ src/components/carouselview/CarouselView.js
@@ -1,7 +1,7 @@
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import React, { Component } from 'react';
 import './CarouselView.css';
-import picturemain from './test4.jpg';
+import picturemain from './picturemain.jpg';
 import picture1 from './picture1.jpg';
 import picture2 from './picture2.jpg';
 import picture3 from './picture3.jpg';
