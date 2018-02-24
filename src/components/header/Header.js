8b95ec2bf339165dc39eeae1a9cde5b4ae81aaed
Viranpat Vongswasdi <viranpat072@hotmail.com>
2018-02-25 03:25:22 +0700

    fix navigation style and breakpoints

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -38,6 +38,16 @@ componentDidMount(){
                   <div className="menu-text">เมนู</div>
                   <div className="bar"></div>
               </div>
+
+              <div className="regular-menu">
+                <ul className="menu-regular-list">
+                  <li className="menu-list"><Link className="link--surinami" to='/'><span data-letters-l="หน้า" data-letters-r="หลัก">หน้าหลัก</span></Link></li>
+                  <li className="menu-list"><Link className="link--surinami" to='/about'><span data-letters-l="เกี่ยวกับ" data-letters-r="โครงการ">เกี่ยวกับโครงการ</span></Link></li>
+                  <li className="menu-list"><Link className="link--surinami" to='/download'><span data-letters-l="เอกสาร" data-letters-r="ประชาสัมพันธ์">เอกสารประชาสัมพันธ์</span></Link></li>
+                  <li className="menu-list"><Link className="link--surinami" to='/info'><span data-letters-l="ความก้าวหน้า" data-letters-r="ของโครงการ">ความก้าวหน้าของโครงการ</span></Link></li>
+                  <li className="menu-list"><Link className="link--surinami" to='/contact'><span data-letters-l="ติด" data-letters-r="ต่อ">ติดต่อ</span></Link></li>
+                </ul>
+              </div>
           </nav>
 
           <div className="overlay">
@@ -45,6 +55,7 @@ componentDidMount(){
                 <li className="menu-option option-1"><Link className="link link--surinami" to='/'><span data-letters-l="หน้า" data-letters-r="หลัก">หน้าหลัก</span></Link></li>
                 <li className="menu-option option-2"><Link className="link link--surinami" to='/about'><span data-letters-l="เกี่ยวกับ" data-letters-r="โครงการ">เกี่ยวกับโครงการ</span></Link></li>
                 <li className="menu-option option-6"><Link className="link link--surinami" to='/download'><span data-letters-l="เอกสาร" data-letters-r="ประชาสัมพันธ์">เอกสารประชาสัมพันธ์</span></Link></li>
+                <li className="menu-option option-7"><Link className="link link--surinami" to='/info'><span data-letters-l="ความก้าวหน้า" data-letters-r="ของโครงการ">ความก้าวหน้าของโครงการ</span></Link></li>
                 <li className="menu-option option-8"><Link className="link link--surinami" to='/contact'><span data-letters-l="ติด" data-letters-r="ต่อ">ติดต่อ</span></Link></li>
               </ul>
           </div>
