b9a3b8d0f38755003f03545515e7678daccf831f
Viranpat Vongswasdi <viranpat072@hotmail.com>
2018-02-25 17:01:50 +0700

    fix router hash

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import CSSTransitionGroup from 'react-addons-css-transition-group';
 import App from './App';
 import './Routes.css';
