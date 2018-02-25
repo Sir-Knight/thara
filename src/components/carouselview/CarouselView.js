36d0a26b6c73424c74139a655f76d4ca25ba60c5
Viranpat Vongswasdi <viranpat072@hotmail.com>
2018-02-25 07:25:32 +0700

    change carousel

--- src/components/carouselview/CarouselView.js
+++ src/components/carouselview/CarouselView.js
@@ -1,135 +1,32 @@
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import React, { Component } from 'react';
 import './CarouselView.css';
-import picturemain from './picturemain.jpg';
-import a3 from './A3.jpg'
-import picture1 from './picture1.jpg';
-import picture2 from './picture2.jpg';
-import picture3 from './picture3.jpg';
-import picture4 from './picture4.jpg';
-import picture5 from './picture5.jpg';
-import picture6 from './picture6.jpg';
-import picture7 from './picture7.jpg';
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 export default class CarouselView extends Component {
-  constructor(props) {
-   super(props);
-   this.state = {
-     items: [
-       a3,
-       picturemain,
-       picture1,
-       picture2,
-       picture3,
-       picture4,
-       picture5,
-       picture6,
-       picture7
-     ],
-     current: 0,
-     isNext: true
-   };
-
-   this.handlerPrev = this.handlerPrev.bind(this);
-   this.handlerNext = this.handlerNext.bind(this);
-   this.goToHistoryClick = this.goToHistoryClick.bind(this);
- }
-
- handlerPrev() {
-   let index = this.state.current,
-       length = this.state.items.length;
-
-   if( index < 1 ) {
-     index = length;
-   }
-
-   index = index - 1;
-
-   this.setState({
-     current: index,
-     isNext: false
-   });
- }
-
- handlerNext() {
-   let index = this.state.current,
-       length = this.state.items.length - 1;
-
-   if( index === length ) {
-     index = -1;
-   }
-
-   index = index + 1;
-
-   this.setState({
-     current: index,
-     isNext: true
-   });
- }
-
- goToHistoryClick( curIndex, index ) {
-   let next = (curIndex < index);
-   this.setState({
-     current: index,
-     isNext: next
-   });
- }
 
  render(){
-   let index = this.state.current,
-       isnext = this.state.isNext,
-       src = this.state.items[index];
-
    return (
      <div className="carousel-view">
-       <div className="carousel">
-        <ReactCSSTransitionGroup
-           transitionName={{
-           enter: isnext ? 'enter-next' : 'enter-prev',
-           enterActive: 'enter-active',
-           leave: 'leave',
-           leaveActive: isnext ? 'leave-active-next' : 'leave-active-prev'
-         }}
-         transitionEnterTimeout={500}
-         transitionLeaveTimeout={500}
-          >
-            <div className="carousel_slide" key={index}>
-              <img src={src}/>
-            </div>
-          </ReactCSSTransitionGroup>
-          <button className="carousel_control carousel_control__prev" onClick={this.handlerPrev}><span></span></button>
-          <button className="carousel_control carousel_control__next" onClick={this.handlerNext}><span></span></button>
-         <div className="carousel_history">
-           <History
-             current={this.state.current}
-             items={this.state.items}
-             changeSilde={this.goToHistoryClick}
-           />
-          </div>
-         </div>
-     </div>
-   )
- }
-}
-
-class History extends React.Component {
-
- render() {
-   let current = this.props.current;
-   let items = this.props.items.map( (el, index) => {
-     let name = (index === current) ? 'active' : '';
-     return (
-       <li key={index}>
-         <button
-           className={name}
-           onClick={ () => this.props.changeSilde(current, index) }
-         ></button>
-       </li>
-     )
-   });
-
-   return (
-     <ul>{items}</ul>
+       <Carousel className="carousel-wrapper" autoPlay showThumbs={false} transitionTime={700} infiniteLoop={true} showStatus={false} showIndicators={false} stopOnHover={false}>
+           <div>
+             <img src="https://image.ibb.co/kd3GoH/resized_resized_1.jpg" alt="resized_resized_1" border="0" />
+           </div>
+           <div>
+             <img src="https://image.ibb.co/fsnLFx/resized_resized_2.jpg" alt="resized_resized_2" border="0" />
+           </div>
+           <div>
+            <img src="https://image.ibb.co/hgMQhc/resized_resized_3.jpg" alt="resized_resized_3" border="0" />
+           </div>
+           <div>
+            <img src="https://image.ibb.co/d65khc/resized_resized_4.jpg" alt="resized_resized_4" border="0" />
+           </div>
+           <div>
+            <img src="https://image.ibb.co/fZEd2c/resized_resized_5.jpg" alt="resized_resized_5" border="0" />
+           </div>
+        </Carousel>
+      </div>
    )
  }
 }
