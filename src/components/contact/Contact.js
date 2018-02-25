362f8257ae6edae987aa1c063167a6c348fbdbf5
Viranpat Vongswasdi <viranpat072@hotmail.com>
2018-02-26 01:45:12 +0700

    change contact layout and content

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,85 +1,135 @@
 import React, { Component } from 'react';
 import './Contact.css';
+import $ from 'jquery';
 import tharaicon from './thara_logo.png';
 import tescoicon from './tesco_logo.png';
 import officeicon from './office_logo.png';
 
 export default class Contact extends Component {
 
-  render() {
-    return (
-        <div className="Contact">
 
-<div id="container">
-   <div id="demo">
+  componentDidMount() {
 
-  <h1 className="padabove">
-   ติดต่อเรา
-  </h1>
-  </div>
+      var head = $("header");
+      var stick = "sticky";
+      var bottom = $(".bottom");
+      var disappear = "sticky-bottom";
 
-  <div id="contact-wrap">
-		<div id="contact-area">
+      $(".contact-frame").scroll(function() {
+          $(".contact-frame").scrollTop() > 300
+              ? head.addClass(stick) + bottom.addClass(disappear)
+              : head.removeClass(stick) + bottom.removeClass(disappear)
+      })
 
-			<form method="post" action="contactengine.php">
-
-
-				<input type="text" name="Name" id="Name" placeholder="ชื่อ นามสกุล" />
-
-				<input type="text" name="City" id="City" placeholder="ที่อยู่"/>
-
-				<input type="text" name="Email" id="Email" placeholder="อีเมลล์"/>
-
-				<input type="text" name="Phone" id="Phone" placeholder="เบอร์โทรศัพ"/>
-
-				<textarea name="Message" rows="20" cols="20" id="Message" placeholder="ข้อความ"></textarea>
-
-				<input type="submit" name="submit" value="Submit" class="submit-button"/>
-			</form>
-        </div>
-     </div>
-     <div className="company">
-       <div className="office">
-        <img src={officeicon}  className="office" alt="office" />
-        <p>สำนักงานสิ่งแวดล้อมและการมีส่วนร่วมของประชาชน
-            2/486 ถนนศรีอยุธยา แขวงทุ่งพญาไท
-            เขตราชเทวี กรุงเทพมหานคร  10400</p>
-<p>โทรศัพท์ : ๐ ๒๓๕๔ ๖๗๗๗</p>
-<p>โทรสาร : ๐ ๒๓๕๔ ๖๗๗๗</p>
-       </div>
-
-       <div className="thara">
-       <img src={tharaicon}  className="thara" alt="thara" />
-       <p>บริษัท ธารา ไลน์ จำกัด
-113 ซอยรัตนาธิเบศร์ 24 ถนนรัตนาธิเบศร์
-ตำบลบางกะสอ อำเภอเมืองนนทบุรี จังหวัดนนทบุรี 1100</p>
-<p>งานด้านสิ่งแวดล้อม</p>
-<p>นายก้องเกียรติ  เรื่องศรี</p>
-<p>โทรศัพท์ : 0 2017 7281 – 2,</p>
-<p>09 7148 0176</p>
-<p>โทรสาร : 0 2017 7882</p>
-<p>งานด้านการมีส่วนร่วมของประชาชน</p>
-<p>นายอภิเชษฐ์  ปานุรัตน์</p>
-<p>โทรศัพท์ : 0 2017 7281 – 2,</p>
-<p>06 2698 7072</p>
-<p>โทรสาร : 0 2017 7882</p>
-       </div>
-       <div className="tesco">
-       <img src={tescoicon}  className="tesco" alt="tesco" />
-       <p>บริษัท เทสโก้ จำกัด
-21/11-14 ซอยสุขุมวิท 18 ถนนสุขุมวิท
-แขวงคลองเตย  เขตคลองเตย กรุงเทพมหานคร 10110</p>
-<p>งานด้านวิศวกรรม</p>
-<p>นางสาวจันทศิริ  สายนภา</p>
-<p>โทรศัพท์ : 0 2258 1320 ต่อ 608,</p>
-<p> 09 0678 6759</p>
-<p>โทรสาร : 0 258 1313</p>
-       </div>
-     </div>
-</div>
-
-
-        </div>
-    );
+      $(".scroll-indicator").click(function() {
+      $(".contact-frame").animate({
+          scrollTop: $(".contact-step").offset().top-200
+      }, 1500);
+  });
   }
+
+    render() {
+
+      return (
+          <div className="contact-frame">
+          <main>
+          <div className="contact-wrapper-background">
+  	<header>ช่องทางติดต่อ</header>
+          </div>
+  <div className="bottom">
+    <div className="scroll-indicator"></div>
+  </div>
+    <div className="content-wrapper-contact">
+    <section className="contact-step">
+
+      <div className="row">
+      <h1>ติดต่อสอบถามข้อมูลเพิ่มเติม</h1>
+        <div className="left-column">
+          <img src={officeicon}  className="office" alt="office" />
+        </div>
+        <div className="right-column">
+            <p>สำนักงานสิ่งแวดล้อมและการมีส่วนร่วมของประชาชน
+                2/486 ถนนศรีอยุธยา แขวงทุ่งพญาไท
+                เขตราชเทวี กรุงเทพมหานคร  10400</p>
+            <p>โทรศัพท์ : 0 2354 2777</p>
+            <p>โทรสาร : 0 2354 2777</p>
+        </div>
+      </div>
+
+
+
+      <div className="row">
+        <h1>บริษัทที่ปรึกษา</h1>
+        <div className="left-column">
+           <img src={tescoicon}  className="tesco" alt="tesco" />
+        </div>
+        <div className="right-column">
+            <p>บริษัท เทสโก้ จำกัด
+             21/11-14 ซอยสุขุมวิท 18 ถนนสุขุมวิท
+             แขวงคลองเตย  เขตคลองเตย กรุงเทพมหานคร 10110</p>
+            <p>โทรศัพท์ : 0 2258 1320</p>
+            <p>โทรสาร : 0 2258 1313</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="left-column">
+          <img src={tharaicon}  className="thara" alt="thara" />
+        </div>
+        <div className="right-column">
+                <p>บริษัท ธารา ไลน์ จำกัด
+         113 ซอยรัตนาธิเบศร์ 24 ถนนรัตนาธิเบศร์
+         ตำบลบางกะสอ อำเภอเมืองนนทบุรี จังหวัดนนทบุรี 1100</p>
+            <p>โทรศัพท์ : 0 2017 7281 – 2</p>
+            <p>โทรสาร : 0 2017 7282</p>
+        </div>
+      </div>
+
+
+
+
+      <div className="row">
+        <div className="left-column">
+          <h2>ด้านวิศวกรรม</h2>
+        </div>
+        <div className="right-column">
+            <p>นางสาวจันทศิริ  สายนภา</p>
+            <p>โทรศัพท์ : 0 2258 1320 - 608, 09 0678 6759</p>
+            <p>โทรสาร : 0 258 1313</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="left-column">
+          <h2>ด้านการมีส่วนร่วมของประชาชน</h2>
+        </div>
+        <div className="right-column">
+            <p>นายอภิเชษฐ์  ปานุรัตน์</p>
+            <p>โทรศัพท์ : 0 2017 7281 – 2, 06 2698 7072</p>
+            <p>โทรสาร : 0 2017 7882</p>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="left-column">
+          <h2>ด้านสิ่งแวดล้อม</h2>
+        </div>
+        <div className="right-column">
+              <p>งานด้านสิ่งแวดล้อม</p>
+              <p>นายก้องเกียรติ  เรื่องศรี</p>
+              <p>โทรศัพท์ : 0 2017 7281 – 2, 09 7148 0176</p>
+              <p>โทรสาร : 0 2017 7882</p>
+        </div>
+      </div>
+
+
+
+
+    </section>
+    </div>
+  </main>
+
+          </div>
+      );
+    }
+
 }
